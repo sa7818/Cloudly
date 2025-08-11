57ee46c942f01d6e98e5cbe20fdf17148fa35522
github.shs@innoveremedical.com <116824700+innogithubshs@users.noreply.github.com>
2025-08-11 09:26:29 -0400

    Initial Commit

---
+++ Cloudly/CloudlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudlyApp.swift
+//  Cloudly
+//
+//  Created by Sara on 2025-08-11.
+//
+
+import SwiftUI
+
+@main
+struct CloudlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cloudly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cloudly
+//
+//  Created by Sara on 2025-08-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
